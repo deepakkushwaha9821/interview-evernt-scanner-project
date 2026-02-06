38cedc0fd6a11c43945af473bec357adcdcea7a5
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2026-02-06 10:08:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Result from "./pages/Result";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Interview />} />
+      {/* <Route path="/" element={<Interview />} /> */}
       <Route path="/mobile" element={<MobileJoin />} />
-      <Route path="/result" element={<Result />} />
+      {/* <Route path="/result" element={<Result />} /> */}
     </Routes>
   );
 }
