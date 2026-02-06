265b23b189d685468ca7f6e45bbdf6cb940bd86d
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2026-02-06 10:21:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Interview from "./pages/Interview";
 import MobileJoin from "./pages/MobileJoin";
-import Result from "./pages/Result";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Interview />} />
       <Route path="/mobile" element={<MobileJoin />} />
-      <Route path="/result" element={<Result />} /> 
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
